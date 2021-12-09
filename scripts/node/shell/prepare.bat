81fe4e197fff21f45ea3b3a481410a9003f98068
Marco Moschettini <m.marmos@gmail.com>
2021-12-09 15:26:33 +0100

    fix windows build

--- scripts/node/shell/prepare.bat
+++ scripts/node/shell/prepare.bat
@@ -6,7 +6,7 @@ tar -C openssl --strip-components=1 -xzf openssl.tar
 del openssl.tar
 
 cd openssl
-perl Configure %OPENSSL_COMPILER% no-shared no-asm no-bf no-camellia no-cast no-cms no-des no-dh no-ec no-ecdh no-ecdsa no-err no-hw no-idea no-jpake no-krb5 no-md2 no-md4 no-mdc2 no-rc2 no-rc4 no-rc5 no-ripemd no-seed no-ssl2 no-ssl3
+perl Configure %OPENSSL_COMPILER% no-shared no-asm no-bf no-camellia no-cast no-cms no-des no-dh no-ec no-ecdh no-ecdsa no-err no-idea no-md2 no-md4 no-mdc2 no-rc2 no-rc4 no-rc5 no-seed no-ssl3
 
 IF /I "%OPENSSL_COMPILER%"=="VC-WIN32" call ms\do_ms
 IF /I "%OPENSSL_COMPILER%"=="VC-WIN64A" call ms\do_win64a
