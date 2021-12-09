bdb83e74db2edd77625bfc25caa9f40e17caf42f
Marco Moschettini <m.marmos@gmail.com>
2021-12-09 15:14:54 +0100

    chore(macos): add support for M1 CPUs

--- scripts/node/shell/prepare.sh
+++ scripts/node/shell/prepare.sh
@@ -10,4 +10,4 @@ curl -s https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz | tar -C 
 cd openssl
 ./Configure $OPENSSL_COMPILER shared no-asm no-bf no-camellia no-cast no-cms no-des no-dh no-ec no-ecdh no-ecdsa no-err no-idea no-md2 no-md4 no-mdc2 no-rc2 no-rc4 no-rc5 no-seed no-ssl3 $MACOS_DEPLOYMENT_VERSION
 make depend
-make build_crypto
+make
