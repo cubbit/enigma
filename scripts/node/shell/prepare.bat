5f67986f29d49709592d8f193cd270e198f69265
Alessio Paccoia <alessio.paccoia@cubbit.io>
2021-12-09 16:33:21 +0100

    fix: compile on windows

--- scripts/node/shell/prepare.bat
+++ scripts/node/shell/prepare.bat
@@ -8,7 +8,4 @@ del openssl.tar
 cd openssl
 perl Configure %OPENSSL_COMPILER% no-shared no-asm no-bf no-camellia no-cast no-cms no-des no-dh no-ec no-ecdh no-ecdsa no-err no-idea no-md2 no-md4 no-mdc2 no-rc2 no-rc4 no-rc5 no-seed no-ssl3
 
-IF /I "%OPENSSL_COMPILER%"=="VC-WIN32" call ms\do_ms
-IF /I "%OPENSSL_COMPILER%"=="VC-WIN64A" call ms\do_win64a
-
-nmake -f ms\nt.mak
+nmake
